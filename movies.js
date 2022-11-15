7aaf5bb568aea4215bcda34f0df1f24229a6132b
Ardit Rrushaj <arditrrushaj@gmail.com>
2022-11-14 18:46:39 -0600

    Finish

--- movies.js
+++ movies.js
@@ -1,13 +1,25 @@
 "use strict";
 
-$("form").on("submit", function(event){
+$("form").on("submit", addMovie);
+
+/**
+ * Removes list item from DOM
+ */
+$("#ratings").on("click","button",function(event){
+    $(event.target).parent().remove();
+})
+
+/**
+ *  Takes inputs from form and adds them to list in DOM
+ */
+function addMovie(event) {
     event.preventDefault();
-    const title = $("movie-name").val();
-    const rating = $("movie-rating").val();
+    const title = $("#movie-name").val();
+    const rating = $("#movie-rating").val();
     const titleRatingCombo = `${title}: ${rating} out of 10`;
 
     const deleteButton = $("<button>").text("X");
 
     const movieListItem = $("<li>").append(titleRatingCombo, deleteButton);
     $("#rating-list").append(movieListItem);
-})
+}
