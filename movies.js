eb45b1926d6af0b9e99926cb1839db3b644315e9
Nico Martinucci <domenico.martinucci@gmail.com>
2022-11-15 09:32:33 -0800

    final commit

--- movies.js
+++ movies.js
@@ -1,25 +1,47 @@
 "use strict";
 
-$("form").on("submit", addMovie);
+const $ratingSlider = $("#movie-rating");
+const $ratingSliderText = $("#rating-display");
 
-/**
- * Removes list item from DOM
- */
-$("#ratings").on("click","button",function(event){
-    $(event.target).parent().remove();
-})
+$ratingSlider.on("change", updateRatingDisplay);
 
-/**
- *  Takes inputs from form and adds them to list in DOM
- */
-function addMovie(event) {
+$("form").on("submit", event => {
     event.preventDefault();
+    addMovie();
+});
+
+$("#ratings").on("click", "button", deleteMovie)
+
+function updateRatingDisplay() {
+    $ratingSliderText.text($ratingSlider.val());
+}
+
+/**
+ * Builds a string from the movie title and rating input
+ * @returns string created from form inputs for movie title and rating
+ */
+function buildMovieStringFromInput() {
     const title = $("#movie-name").val();
     const rating = $("#movie-rating").val();
     const titleRatingCombo = `${title}: ${rating} out of 10`;
 
+    return titleRatingCombo;
+}
+
+/**
+ *  Takes inputs from form and adds them to list in DOM
+ */
+function addMovie() {
     const deleteButton = $("<button>").text("X");
 
-    const movieListItem = $("<li>").append(titleRatingCombo, deleteButton);
+    const movieListItem = $("<li>").append(buildMovieStringFromInput, deleteButton);
+
     $("#rating-list").append(movieListItem);
+}
+
+/**
+ * Removes list item from DOM
+ */
+function deleteMovie(event) {
+    $(event.target).parent().remove();
 }
